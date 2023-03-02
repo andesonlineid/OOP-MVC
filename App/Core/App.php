1364b85620b86d84606acb20f0a188a62dace0d4
brians dream <destarock1@gmail.com>
2023-03-02 11:32:57 +0700

    adding routing implementation - MVC concept

--- App/Core/App.php
+++ App/Core/App.php
@@ -1,12 +1,23 @@
 <?php 
 
 class App {
-    
+
     // constructor
     public function __construct()
     {
-        echo "test mvc folder organisation";
+       $url = $this->parseUrl();
+       var_dump($url);
         
+    } 
+    public function parseURL() {
+        if(isset($_GET["url"])) {
+        $url = rtrim($_GET["url"],'/');
+        $url = filter_var($url,FILTER_SANITIZE_URL);
+        $url = explode('/',$url);    
+        return $url;
+
+        }
+
     }
 
 }
--- App/init.php
+++ App/init.php
@@ -1,4 +1,4 @@
 <?php 
-require_once "Core/App.php";
+require_once "core/app.php";
 
 ?>
