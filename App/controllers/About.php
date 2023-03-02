e9b6b144b89f045245dedde601163e7a5cfdb0d9
brians dream <destarock1@gmail.com>
2023-03-02 16:59:33 +0700

    adding controller implementation MVC concept

---
+++ App/controllers/About.php
@@ -0,0 +1,12 @@
+<?php 
+
+    class About {
+        public function index($name = "andes", $job="Web Programmer") {
+            echo " 
+            Hello I am $name  and my job is $job
+            \n this is default controller About/index";
+        }
+      
+    }
+
+?>
--- App/init.php
+++ App/init.php
@@ -1,4 +1,4 @@
 <?php 
-require_once "core/app.php";
+require_once "core/App.php";
 
 ?>
